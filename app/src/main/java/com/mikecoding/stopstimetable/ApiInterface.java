704632e6f8a9549fe3edc1724e4b40aedebe5c8e
Mike <michael.nyberghjerto@outlook.com>
2017-05-08 12:52:15 +0200

    Main layout - creating apicaller interface, stationadapter done, create second activity files and intent

---
+++ app/src/main/java/com/mikecoding/stopstimetable/ApiInterface.java
@@ -0,0 +1,7 @@
+package com.mikecoding.stopstimetable;
+
+import java.util.ArrayList;
+
+public interface ApiInterface {
+    public void onTaskComplete(ArrayList<Station> stations);
+}
--- app/src/main/java/com/mikecoding/stopstimetable/StationAdapter.java
+++ app/src/main/java/com/mikecoding/stopstimetable/StationAdapter.java
@@ -1,15 +1,49 @@
 package com.mikecoding.stopstimetable;
 
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class StationAdapter {
+public class StationAdapter extends ArrayAdapter<Station> {
     Context context;
     int layoutResourceId;
-    //ArrayList<Station> stations = null;
+    ArrayList<Station> stations = null;
 
+    public StationAdapter(Context context, int layoutResourceId, ArrayList<Station> stations) {
+        super(context, layoutResourceId, stations);
+        this.context = context;
+        this.layoutResourceId = layoutResourceId;
+        this.stations = (ArrayList) stations;
+    }
 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        View row = convertView;
+        StationHolder holder = null;
 
+        if (row == null){
+            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);
+
+            holder = new StationHolder();
+            holder.name = (TextView) row.findViewById(R.id.name);
+            row.setTag(holder);
+        }else{
+            holder = (StationHolder) row.getTag();
+        }
+        Station item = stations.get(position);
+        holder.name.setText(item.getName());
+        return row;
+    }
+
+    private class StationHolder {
+        public TextView name;
+    }
 }
