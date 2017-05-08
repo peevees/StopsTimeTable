614691dd1c4bbd08344737ad14bbf7cba3b3e2b4
Mike <michael.nyberghjerto@outlook.com>
2017-05-08 10:15:37 +0200

    Main layout - creating ListView adapters

--- app/src/main/java/com/mikecoding/stopstimetable/StationAdapter.java
+++ app/src/main/java/com/mikecoding/stopstimetable/StationAdapter.java
@@ -1,6 +1,15 @@
 package com.mikecoding.stopstimetable;
 
 
+import android.content.Context;
+
+import java.util.ArrayList;
+
 public class StationAdapter {
+    Context context;
+    int layoutResourceId;
+    //ArrayList<Station> stations = null;
+
+
 
 }
